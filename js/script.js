7483b9ba7751768aa9ae4ad0a1c772233059910e
Paulo Ramon Nogueira de Freitas <pramon71@gmail.com>
2021-05-11 20:22:04 -0300

    Parte 5 - Recriando o jogo da cobrinha com JavaScript

--- js/script.js
+++ js/script.js
@@ -46,6 +46,13 @@ function iniciarJogo() {
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(let i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert(':( - Game Over!');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     criarComida();
@@ -58,7 +65,14 @@ function iniciarJogo() {
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    /* Verificar se cobrinha come a comida */
+    if(snakeX != comida.x || snakeY != comida.y) {
+        snake.pop();
+    } else {
+        comida.x = Math.floor(Math.random() * 15 + 1) * box;
+        comida.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+    
 
     let newHead = {
         x: snakeX,
